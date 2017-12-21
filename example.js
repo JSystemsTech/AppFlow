2e0471731a9e2c089e6aa70906e894f73b66c889
JSystemsTech <jsystemstech@gmail.com>
2017-12-21 11:20:19 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,19 @@
 
 $( document ).ready(function() {
+    var TRAY_CLASSES = {
+  spacing: 'spacing-1 spacing-2 spacing-3 spacing-4 spacing-5',
+  'app-size': 'app-size-1 app-size-2 app-size-3 app-size-4 app-size-5 app-size-6 app-size-7 app-size-8'
+ }
+    var changeTrayClass = function(tray, type, added){
+  tray.removeClass(TRAY_CLASSES[type]).addClass(added);
+ }
+    console.log($('#MyAppTrayControls input[name="spacing"]'));
 $('#MyAppTrayControls input[name="spacing"]').bind('change',function(){
+    console.log('change');
     changeTrayClass($('#myAppTray'), 'spacing', $('#form1 input[name="spacing"]:checked').val());
 });
     $('#MyAppTrayControls input[name="app-size"]').bind('change',function(){
     changeTrayClass($('#myAppTray'), 'app-size', $('#form1 input[name="app-size"]:checked').val());
 });
- var TRAY_CLASSES = {
-  spacing: 'spacing-1 spacing-2 spacing-3 spacing-4 spacing-5',
-  'app-size': 'app-size-1 app-size-2 app-size-3 app-size-4 app-size-5 app-size-6 app-size-7 app-size-8'
- }
- var changeTrayClass = function(tray, type, added){
-  tray.removeClass(TRAY_CLASSES[type]).addClass(added);
- }
+ 
  });
