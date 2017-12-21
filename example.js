e72e16adbc97728dbf6cd25046911fbb6325cd87
JSystemsTech <jsystemstech@gmail.com>
2017-12-21 12:52:46 -0500

    Update example.js

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 $( document ).ready(function() {
     var TRAY_CLASSES = {
   spacing: 'spacing-1 spacing-2 spacing-3 spacing-4 spacing-5',
-  'app-size': 'app-size-1 app-size-2 app-size-3 app-size-4 app-size-5 app-size-6 app-size-7 app-size-8'
+  'app-size': 'app-size-1 app-size-2 app-size-3 app-size-4 app-size-5 app-size-6 app-size-7 app-size-8 app-size-auto'
  }
     var changeTrayClass = function(tray, type, added){
         tray.removeClass(TRAY_CLASSES[type]).addClass(added);
