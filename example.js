d9e2cdd419fa6199251c2f4b9618d84081e91343
JSystemsTech <jsystemstech@gmail.com>
2017-12-22 18:36:03 -0500

    Update example.js

--- example.js
+++ example.js
@@ -5,9 +5,9 @@ $( document ).ready(function() {
   'app-size': 'app-size-1 app-size-2 app-size-3 app-size-4 app-size-5 app-size-6 app-size-7 app-size-8 app-size-auto',
   'app-span': 'app-span-1 app-span-2 app-span-3 app-span-4 app-span-5',
   'app-height-span': 'app-height-span-2 app-height-span-3 app-height-span-4 app-height-span-5',
-  'icon-size':'icon-size-xs icon-size-sm icon-size-md icon-size-lg icon-size-xl',
+  'icon-size':'app-icon-xs app-icon-sm app-icon-md app-icon-lg app-icon-xl',
         'glass': 'glass',
-        'square': 'square'
+        'square': 'app-square'
  }
     var changeElClass = function(el, type, added){
         el.removeClass(El_CLASSES[type]).addClass(added);
@@ -22,7 +22,7 @@ $('#MyAppTrayControls input[name="spacing"]').bind('change',function(){
     $('#myAppTray').find('> .app').each(function(){
           var app = $(this);
           var appSettingsForm = app.find('> .app-content form.app-settings');
-        console.log(appSettingsForm);
+        
           var glassCheckbox = appSettingsForm.find('input[name="glass"]');
           var squareCheckbox = appSettingsForm.find('input[name="square"]');
           var iconSizeRadio = appSettingsForm.find('input[name="icon-size"]');
@@ -48,7 +48,7 @@ $('#MyAppTrayControls input[name="spacing"]').bind('change',function(){
         squareCheckbox.bind('change',function(){
             var changeClass = '';
             if(appSettingsForm.find('input[name="square"]').prop('checked')){
-            changeClass = 'square';
+            changeClass = 'app-square';
                }
             changeElClass(app, 'square', changeClass);
         });
