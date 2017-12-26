f7c882214c95a443173123acd5fea39dee342ba4
JSystemsTech <jsystemstech@gmail.com>
2017-12-26 13:36:25 -0500

    Update SampleSite.js

--- SampleSite.js
+++ SampleSite.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
  var tray = $('#MainTray');
+ console.log(tray.find('.open-cart'));
  tray.find('.open-cart').bind('click', function(){
     $('#cart').trigger('app-open');
  });
