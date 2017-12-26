9972767d666a8c3ad1506b185bcb3de3c70ba0aa
JSystemsTech <jsystemstech@gmail.com>
2017-12-26 13:34:05 -0500

    Update SampleSite.js

--- SampleSite.js
+++ SampleSite.js
@@ -1,8 +1,6 @@
 $( document ).ready(function() {
  var tray = $('#MainTray');
- var cart = $('#cart');
  tray.find('.open-cart').bind('click', function(){
-  console.log(cart);
-    cart.trigger('app-open');
+    $('#cart').trigger('app-open');
  });
 });
