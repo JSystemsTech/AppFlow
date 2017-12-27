ec3fd6bbfe55397e3d6678590aad678f5d42eaaa
JSystemsTech <jsystemstech@gmail.com>
2017-12-27 14:34:47 -0500

    Update AppFlow.js

--- AppFlow.js
+++ AppFlow.js
@@ -56,7 +56,7 @@ var bindAppEvents = function(apps, tray) {
             e.preventDefault();
             var targetApp = $(e.target).closest('.app');
             var closingApp = $(e.target).closest('.app-close').closest('.app').is(targetApp) && 
-                             $(e.target).('[data-toggle-app]').closest('.app').is(targetApp);
+                             $(e.target).closest('[data-toggle-app]').closest('.app').is(targetApp);
             var isActive = $(e.target).hasClass(ACTIVE_CLASS);
             if(!isActive && !closingApp){
                  openApp(targetApp, tray);
