f8d59c7f32b84b65aa16f92c0682c6e5d31a5e01
JSystemsTech <jsystemstech@gmail.com>
2018-01-09 11:16:52 -0500

    Update AppFlow.js

--- AppFlow.js
+++ AppFlow.js
@@ -72,7 +72,7 @@ var bindAppEvents = function(apps, tray) {
         'keypress': function(e) {
             if (e.which === 32){
                 console.log($(this));
-                $(this).trigger('click');
+                $($(this)[0]).trigger('click');
                 e.preventDefault();
             }
         }
